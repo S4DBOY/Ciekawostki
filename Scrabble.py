fdc8b4334ecd005a806bca5fe54a461b5700b3ff
SadBoy <kim210@wp.pl>
2018-01-08 00:01:13 +0100

    Działa XD
    Pisane pod Python 3

--- Scrabble.py
+++ Scrabble.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
-
 from itertools import permutations
-
-letters = raw_input('Jakie literki tam sie zawieraja? ')
-count_word = int(raw_input('Iloliterowe jest to slowo? '))
+letters = input("Jakie literki tam sie zawieraja? ")
+count_word = int(input("Iloliterowe jest to slowo? "))
 words = {''.join(x) for x in permutations(letters,count_word)}
 
 with open('slowa.txt') as f:
 	for linia in f:
 		if linia.strip() in words:
-			print linia.strip()
+			if len(linia.strip()) == count_word:
+				print (linia.strip())

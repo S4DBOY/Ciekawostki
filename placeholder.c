f8c19c59e0196f1d5238e16fb801ce875a357c1e
SadBoy <kim210@wp.pl>
2018-12-22 20:05:00 +0100

    Zwracanie łańcucha znaków do wskaźnika.

---
+++ placeholder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* function to generate and return random numbers */
+int * getRandom( ) {
+
+   static char  r[10] = "kamil";
+      printf( "r = \"%s\"\n",r);
+
+   return r;
+}
+
+/* main function to call above defined function */
+int main () {
+
+   /* a pointer to an int */
+   char *p;
+   int i;
+
+   p = getRandom();
+   sprintf(p,"%sszwa",p);
+    printf( "*(p) : %s\n",p);
+    free(p);
+   return 0;
+}
